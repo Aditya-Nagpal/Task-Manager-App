6c4d97f02066eb693ca1ff5fb5a79b37ad2ed092
Aditya-Nagpal <adityanagpal10@gmail.com>
2023-08-12 19:46:04 +0530

    commit

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express=require('express');
 const app=express();
+const path=require('path');
+const bodyParser=require('body-parser');
 const port=9000;
 
-app.use('/',require('./routes/index'));
-
 app.set('view engine','ejs');
-app.set('views','./views');
+app.set('views',path.join(__dirname,'views'));
+app.use(express.urlencoded());
+app.use(express.static('/assets'));
+
+app.use('/',require('./routes/index'));
 
 app.listen(port, function(err){
     if(err){
